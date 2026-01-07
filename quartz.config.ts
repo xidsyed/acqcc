27f7d2e8e007d160a87d07f6f5d8c713b8e79203
xid <26184016+xidsyed@users.noreply.github.com>
2026-01-07 19:06:24 +0530

    updated domain name

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "acqcc",
+    pageTitle: "sidequests",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "xidsyed.github.io/acqcc",
+    baseUrl: "syedmohsin.blog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
